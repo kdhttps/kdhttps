aea6d4b4a455925fa1e5422808383328bbcb87b2
kdhttps <kdhttps@gmail.com>
2020-06-22 17:59:06 +0530

    fix: Fixed routing issue

--- scully.kdhttps.config.ts
+++ scully.kdhttps.config.ts
@@ -6,13 +6,13 @@ export const config: ScullyConfig = {
   routes: {
     "/blog/:id/:slug": {
       type: 'json',
-      slug: {
-        url: 'https://mean-star.herokuapp.com/blogs/publisher/5ec7cc7eee39a5001244805b',
-        property: 'title'
-      },
       id: {
         url: 'https://mean-star.herokuapp.com/blogs/publisher/5ec7cc7eee39a5001244805b',
         property: '_id'
+      },
+      slug: {
+        url: 'https://mean-star.herokuapp.com/blogs/publisher/5ec7cc7eee39a5001244805b',
+        property: 'title'
       },
     }
   }
