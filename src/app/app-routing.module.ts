0be87c4b75642b07410e191e7c2126678285be25
kdhttps <kdhttps@gmail.com>
2020-06-24 22:58:06 +0530

    feat: Fixed blog title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-
 const routes: Routes = [
-  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
   { path: '', loadChildren: () => import('./blog/blog.module').then(m => m.BlogModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
 ];
 
 @NgModule({
