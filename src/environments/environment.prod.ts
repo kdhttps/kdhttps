3a60d0673f1af7a9c28f54f65723295071c82447
kdhttps <kdhttps@gmail.com>
2023-06-08 15:58:26 +0530

    chore: rebuild

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  assetsURL: 'https://raw.githubusercontent.com/kdhttps/generator/master/assets',
-  blogsUrl: 'https://mean-star.herokuapp.com/blogs',
-};
